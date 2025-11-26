cc0a9475a63fe25c538669e73b98940f72bbcea1
tongthanaphon <tongthanaphon.tk@gmail.com>
2025-11-26 22:58:36 +0700

    fix: github pages base path

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/p2p-harvest-game/",
   plugins: [react()],
   server: {
     port: 3000,
     fs: {
-      allow: ['..']
-    }
+      allow: [".."],
+    },
   },
   optimizeDeps: {
-    exclude: ['p2p-harvest-game-wasm']
-  }
-})
+    exclude: ["p2p-harvest-game-wasm"],
+  },
+});
